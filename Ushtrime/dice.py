e796c6efa95bdcf1566448a31935c8c97a2cfd80
Rilind Kycyku <r.kycyku.12@gmail.com>
2019-11-15 21:41:18 +0100

    Update dice.py

--- Ushtrime/dice.py
+++ Ushtrime/dice.py
@@ -1,10 +1,14 @@
 from time import sleep
 from random import randint
 
-shakedice = input("Do you want to shake the dice? ")
+shakedice = input("Let's shake some dice! (Y/N) ")
 shakedice = shakedice.lower()
 
-while shakedice == "yes":
+playerpoints = 0
+computerpoints = 0
+
+
+while shakedice == "y":
     d1 = randint(1,6)
     d2 = randint(1,6)
     d3 = randint(1,6)
@@ -13,28 +17,68 @@ while shakedice == "yes":
     player = d1 + d2
     computer = d3 + d4
 
-    print("Your turn to roll")
+    print("\nYour turn to roll")
     sleep(1)
     print("You shook a " + str(d1) + " and " + str(d2) + ", so you have " + str(player))
     sleep(1)
-    print("My turn to roll")
+    print("\nMy turn to roll")
     sleep(1)
-    print("I shook a " + str(d1) + " and " + str(d2) + ", so I have " + str(player))
+    print("I shook a " + str(d3) + " and " + str(d4) + ", so I have " + str(computer))
 
     if player > computer:
+        playerpoints += 1
         sleep(1)
         print("\nYou won")
+        sleep(1)
+        print("\nYou win", str(player) + " to " + str(computer))
+        sleep(1)
     elif player < computer:
+        computerpoints += 1
         sleep(1)
         print("\nI won")
-    else:
+        sleep(1)
+        print("\nI win", str(player) + " to " + str(computer))
+        sleep(1)
+    elif player == computer:
         sleep(1)
         print("\nTie")
-        
-    sleep(1)
-    shakedice = input("\nA deshironi te gjuani perseri? ")
-    shakedice = shakedice.lower()
     
+    if playerpoints > computerpoints:
+        sleep(1)
+        print("\nYou're winning", str(playerpoints) + " to " + str(computerpoints))
+    elif computerpoints > playerpoints:
+        sleep(1)
+        print("\nI'm winning", str(computerpoints) + " to " + str(playerpoints))
+    else:
+        sleep(1)
+        print("\nThe score is tied", str(playerpoints) + " to " + str(computerpoints))
+
+    if playerpoints == 5:
+        print("\nYou win the game!")
+        break
+    elif computerpoints == 5:
+        print("\nI win the game!")
+        break
+
+    shakedice = input("\nCountinue? (Y/N) ")
+    shakedice = shakedice.lower()
+
+    if shakedice == "n":
+        print("\nHave a nice day!")
+        sleep(1)
+        if playerpoints > computerpoints:
+            print("\nYou win the game!")
+            sleep(1)
+            print("You've won", str(playerpoints) + " to " + str(computerpoints))
+        elif computerpoints > playerpoints:
+            print("\nI win the game!")
+            sleep(1)
+            print("I've won ", str(computerpoints) + " to " + str(playerpoints))
+        else:
+            print("\nGame is a Tie!")
+            sleep(1)
+            print("The score is tied", str(playerpoints) + " to " + str(computerpoints))
+
 
 
 
