3505611bae3faafdef9fdce1cd4e5a9aa4ce1951
Rilind Kycyku <r.kycyku.12@gmail.com>
2019-11-19 21:04:52 +0100

    Create todo.py

---
+++ Ushtrime/todo.py
@@ -0,0 +1,18 @@
+print("Task list")
+
+print("\nNO TASKS")
+
+print("\n(1) Add Task")
+print("(2) Delete Task")
+print("(3) Edit Task")
+print("(4) Quit")
+
+a = input("\nWhat do you want to do: ")
+
+lista = {}
+
+if a == 1:
+    task = input("Enter a task: ")
+
+print(1)
+print(2)
